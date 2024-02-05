a501fe276aced2bd74fb067f11933cc92996ff3d
Daniel Jaramillo Moya <da.jaramim@gmail.com>
2024-02-05 12:14:01 -0300

    feat: list of book are now render

--- library.js
+++ library.js
@@ -1,4 +1,8 @@
+booksContainer = document.querySelector('#books-container');
+addBtn = document.querySelector('#add-btn'); 
+
 const myLibrary = [];
+
 
 class Book {
   constructor(title, author, pages, read) {
@@ -11,7 +15,33 @@ class Book {
 
 function addBookToLibrary(book) {
   // do stuff here
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
-addBookToLibrary("harry potter")
+
+function renderLibrary() {
+  myLibrary.forEach(book => {
+    const bookDiv = document.createElement('DIV');
+    const bookTitle = document.createElement('H3');
+    const bookAuthor = document.createElement('P');
+    const bookPages = document.createElement('P');
+    const bookRead = document.createElement('P');
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookRead.textContent = book.read ? "Read" : "Not read";
+
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookPages);
+    bookDiv.appendChild(bookRead);
+
+    booksContainer.appendChild(bookDiv);
+  });
+}
+
+theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", false);
+addBookToLibrary(theHobbit)
 console.log(myLibrary);
+
+renderLibrary()
